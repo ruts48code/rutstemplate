875ef8434a5eb07e84c4c8f61aebbc8166cec149
Authapon R. Kongwan <authapon@gmail.com>
2024-07-22 13:44:53 +0700

    modify web.js to work with PORT env

--- web.js
+++ web.js
@@ -5,13 +5,13 @@ import { handler } from './build/handler.js';
 import polka from 'polka';
 
 const cpu = availableParallelism();
-
+const port = process.env.PORT || 80
 if (cluster.isPrimary) {
 	console.log(`Primary ${process.pid} is running`);
 	for (let i = 0; i < cpu; i++) {
 		cluster.fork();
 	}
 } else {
-	polka().use(handler).listen(4999);
+	polka().use(handler).listen(port);
 	console.log(`Worker ${process.pid} started`);
 }
